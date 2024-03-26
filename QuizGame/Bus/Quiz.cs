82525ac307cf4394433d2c50441c165454a1e1dd
Redsilver2 <64178508+Redsilver2@users.noreply.github.com>
2024-03-26 01:34:34 -0400

    First Commit

---
+++ QuizGame/Bus/Quiz.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace QuizGame.Bus
+{
+    public enum QuizType
+    {
+        TrueOrFalse,
+        MultipleChoice,
+    }
+
+    public struct Quiz
+    {
+        private QuizType type;
+        
+        private string question;
+        private int answerIndex;
+
+        private string[] choices; 
+
+        public Quiz(QuizType type, string question, string[] choices, int answerIndex)
+        {
+            this.type = type;
+            this.question = question;
+            this.choices = choices;
+            this.answerIndex = answerIndex;
+        }
+
+        public void Answer(ref int counter)
+        {
+            string input = string.Empty;
+            char   inputCharacter;
+            char[] indexCharacters = GetIndexCharacters();
+
+            if(answerIndex > choices.Length - 1)
+            {
+                answerIndex = choices.Length - 1;
+            }
+            else if(answerIndex < 0)
+            {
+                answerIndex = 0;
+            }
+
+            Console.WriteLine(GetState());
+
+            while (true)
+            {
+                Console.Write("Entrer une réponse valide: ");
+                input = Console.ReadLine();
+
+                if(input.Length != 1)
+                {
+                    Console.WriteLine("\nCette réponse n'est pas valide!\n");
+                }
+                else
+                {
+                    input = input.ToUpper();
+                    inputCharacter = input[0];
+
+                    if (IsValidAnswer(indexCharacters, inputCharacter))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nCette réponse n'est pas valide!\n");
+                    }
+                }
+            }
+
+            Console.WriteLine($"\nLa bonne réponse était ({indexCharacters[answerIndex].ToString().ToUpper()})");
+
+            if (inputCharacter == indexCharacters[answerIndex])
+            {
+                Console.ForegroundColor = ConsoleColor.Green;//change de couleur  
+                Console.WriteLine("\n\t\tBonne reponse!");
+
+                counter++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;//change de couleur  
+                Console.WriteLine("\n\t\tMauvaise Réponse!");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public string GetState()
+        {
+            string description = $"{this.question}\n\n";
+            char[] indexCharacters = GetIndexCharacters();
+
+            for (int i = 0; i < choices.Length; i++)
+            {
+                char indexCharacter = indexCharacters[i];
+                string choice = choices[i];
+
+                description += $"{indexCharacter}. {choice}\n";
+
+                if(i == indexCharacters.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            return description;
+        }
+
+        private bool IsValidAnswer(char[] characters , char inputCharacter)
+        {
+            for (int i = 0; i < choices.Length; i++)
+            {
+                if(inputCharacter == characters[i])
+                {
+                    return true;
+                }
+
+                if (i == characters.Length - 1)
+                {
+                    break;
+                }
+            }
+
+            return false;
+        }
+
+        private char[] GetIndexCharacters() 
+        {
+            switch (type)
+            {
+                case QuizType.TrueOrFalse:
+                    return new char[] { '1', '2' };
+
+                case QuizType.MultipleChoice:
+                    return new char[] { 'A', 'B', 'C' };
+            }
+
+            return null;
+        }
+    }
+}
---
+++ QuizGame/User/Program.cs
@@ -0,0 +1,110 @@
+﻿using QuizGame.Bus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExiting = false;
+            string[] authorsNames = new string[] { "Marvin" };
+
+            while (!isExiting)
+            {
+                Console.WriteLine("Commande: [Choix] [Index Du Quiz]\n\n" +
+                                  "0. Quittez l'application");
+
+                for (int i = 0; i < authorsNames.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. Quiz de {authorsNames[i]}");
+                }
+
+                Console.Write("\nEntrez un choix: ");
+
+                string[] inputArgs = Console.ReadLine().Split(new char[] { ' ' });
+
+                try
+                {
+                    int index = int.Parse(inputArgs[0]);
+
+                    switch (index)
+                    {
+                        case 0:
+                            isExiting = true;
+                            break;
+
+                        default:
+                            int arg2 = int.Parse(inputArgs[1]);
+                            int counter = 0;
+
+                            if (arg2 < 0 || arg2 >= authorsNames.Length)
+                            {
+                                Console.WriteLine("\nIl y a une erreur! \n");
+                                break;
+                            }
+
+                            string authorName = authorsNames[index - 1];
+                            Quiz[] quizzes = GetQuizByAuthor(authorName, arg2);
+
+                            if (quizzes.Length == 0)
+                            {
+                                Console.WriteLine("\nIl n'y a pas de quiz pour cet autheur.\n");
+                                break;
+                            }
+
+                            Console.WriteLine();
+
+                            foreach (Quiz quiz in quizzes)
+                            {
+                                quiz.Answer(ref counter);
+                                Console.WriteLine();
+                            }
+
+
+                            double result = (counter / (double)quizzes.Length) * 100;
+                            result = Math.Round(result, 2);
+
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"Vous avez eu {counter} bonnes réponses sur {quizzes.Length} \nNote Finale: ({result}%) \n");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("\nIl y a une erreur! \n");
+                }
+            }
+
+            Console.WriteLine("Application créé par ...");
+            Console.ReadKey();
+        }
+
+        private static Quiz[] GetQuizByAuthor(string authorName, int index)
+        {
+            authorName = authorName.ToLower() + $"{index}";
+
+            switch (authorName)
+            {
+                case "marvin0":
+                    return new Quiz[2]
+                    {
+                        new Quiz(QuizType.TrueOrFalse, "Are shrimps faster than dolphins?"
+                                , new string[] {"True", "False" }, 1),
+
+                        new Quiz(QuizType.MultipleChoice, "Which character is more famous?"
+                                , new string[] {"Sonic", "Mario", "Doom Slayer" }, 1)
+                    };
+
+                default:
+                    return new Quiz[0];
+            }
+        }
+    }
+}
